50558023955b5521e378f31c02675ef2eeb66c41
SantiaGhou <filipecacule@gmail.com>
2025-03-28 19:16:27 -0300

    Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar';
 
-import { Header } from './components/header'
-import './global.css'
-import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar'
-function App() {
+import styles from './App.module.css';
 
+import './global.css';
 
-
+export function App() {
   return (
-<div>
- <Header/>
+    <div>
+      <Header />
 
- <div className={styles.wrapper}>
-<Sidebar/>
-<main>
-</main>
- </div>
-</div>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post/>
+          <Post/>
+        </main>
+      </div>
+    </div>
   )
 }
-
-export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css';
+
+ export function Post() {
+   return (
+     <article className={styles.post}>
+       <header>
+         <div className={styles.author}>
+           <img className={styles.avatar} src="https://instagram.fgnm1-1.fna.fbcdn.net/v/t51.2885-19/487037882_622905877385634_9127151639876459389_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.fgnm1-1.fna.fbcdn.net&_nc_cat=106&_nc_oc=Q6cZ2QE7-FUd60tXItU14GI2VV1oYhDeUjR68wqXmxlVgu2oiUGBqN1Vs6yfealkU7m_F-o&_nc_ohc=_r3fr6FoKDMQ7kNvgE-eCBa&_nc_gid=NTBmBrQHNco4zqtBo4ag_g&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYFC1_pqmbMCXTy2PxR4kh-3pLqbn9-CTxp67ZFnwv8mDQ&oe=67ECF262&_nc_sid=7a9f4b" />
+           <div className={styles.authorInfo}>
+             <strong>Raphael</strong>
+             <span>Web Developer</span>
+           </div>
+         </div>
+ 
+         <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+       </header>
+ 
+       <div className={styles.content}>
+         <p>Fala galeraa 👋</p>
+         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+         <p>👉<a href="">jane.design/doctorcare</a></p>
+         <p><a href="">#novoprojeto #nlw #react</a></p>
+       </div>
+     </article>
+   )
+ }
