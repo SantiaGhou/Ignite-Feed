e68c4bc46b5ca3e2a298f5d2883a82b31dce149d
SantiaGhou <filipecacule@gmail.com>
2025-03-28 13:25:00 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import { Header } from './components/header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 function App() {
+
 
 
   return (
 <div>
  <Header/>
+
+ <div className={styles.wrapper}>
+<Sidebar/>
+<main>
+</main>
+ </div>
 </div>
   )
 }
